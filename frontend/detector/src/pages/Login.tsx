88cfca992d907ce95c8ffa84d486bf9e3ed51a56
Supremo1717 <ralphmatthewmanabat@gmail.com>
2025-03-26 18:19:32 +0800

    up

--- frontend/detector/src/pages/Login.tsx
+++ frontend/detector/src/pages/Login.tsx
@@ -214,7 +214,7 @@ export const LoginPage = () => {
               </a> */}
             </div>
             <div className="relative">
-            <div style={{cursor:"pointer"}}  className="z-100  absolute top-3 inset-y-0 end-0 pointer-events-none pe-3">
+            <div style={{cursor:"pointer"}}  className="z-100  absolute top-3 inset-y-0 end-0  pe-3">
               {seePassword? <svg onClick={()=>setSeePassword(false)} style={{cursor:"pointer"}} height={24} width={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 16.01C14.2091 16.01 16 14.2191 16 12.01C16 9.80087 14.2091 8.01001 12 8.01001C9.79086 8.01001 8 9.80087 8 12.01C8 14.2191 9.79086 16.01 12 16.01Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M2 11.98C8.09 1.31996 15.91 1.32996 22 11.98" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M22 12.01C15.91 22.67 8.09 22.66 2 12.01" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>:
               <svg onClick={()=>setSeePassword(true)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
               <g id="SVGRepo_bgCarrier" strokeWidth={0} />
