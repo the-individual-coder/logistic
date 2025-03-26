517c5bba6e695994890478a5c8ea3928e4589407
Supremo1717 <ralphmatthewmanabat@gmail.com>
2025-03-26 16:24:34 +0800

    add eye for password

--- frontend/detector/src/pages/Login.tsx
+++ frontend/detector/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react"
+import { useEffect, useState , useRef } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FadeLoader } from "react-spinners";
@@ -7,9 +7,11 @@ export const LoginPage = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('')
+  const [seePassword, setSeePassword] = useState(false)
   const [wrongCredentials, setWrongCredentials] = useState(false)
   const [rememberMe, setRememberMe] = useState(false)
   const navigate = useNavigate()
+  const eye = useRef<HTMLInputElement>(null)
   const hostServer = import.meta.env.VITE_SERVER_HOST
   useEffect(()=>{
     authCheck()
@@ -28,6 +30,14 @@ export const LoginPage = () => {
 
     }
   },[])
+  useEffect(()=>{
+
+      if(eye.current) {
+        if(seePassword)   eye.current.type = 'text' 
+        else eye.current.type = 'password'
+      }
+
+  }, [seePassword])
 
   const authCheck = async () => {
     try {
@@ -204,7 +214,61 @@ export const LoginPage = () => {
               </a> */}
             </div>
             <div className="relative">
+            <div style={{cursor:"pointer"}}  className="z-100  absolute top-3 inset-y-0 end-0 pointer-events-none pe-3">
+              {seePassword? <svg onClick={()=>setSeePassword(false)} style={{cursor:"pointer"}} height={24} width={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 16.01C14.2091 16.01 16 14.2191 16 12.01C16 9.80087 14.2091 8.01001 12 8.01001C9.79086 8.01001 8 9.80087 8 12.01C8 14.2191 9.79086 16.01 12 16.01Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M2 11.98C8.09 1.31996 15.91 1.32996 22 11.98" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M22 12.01C15.91 22.67 8.09 22.66 2 12.01" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>:
+              <svg onClick={()=>setSeePassword(true)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
+              <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  d="M14.83 9.17999C14.2706 8.61995 13.5576 8.23846 12.7813 8.08386C12.0049 7.92926 11.2002 8.00851 10.4689 8.31152C9.73758 8.61453 9.11264 9.12769 8.67316 9.78607C8.23367 10.4444 7.99938 11.2184 8 12.01C7.99916 13.0663 8.41619 14.08 9.16004 14.83"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />{" "}
+                <path
+                  d="M12 16.01C13.0609 16.01 14.0783 15.5886 14.8284 14.8384C15.5786 14.0883 16 13.0709 16 12.01"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />{" "}
+                <path
+                  d="M17.61 6.39004L6.38 17.62C4.6208 15.9966 3.14099 14.0944 2 11.99C6.71 3.76002 12.44 1.89004 17.61 6.39004Z"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />{" "}
+                <path
+                  d="M20.9994 3L17.6094 6.39"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />{" "}
+                <path
+                  d="M6.38 17.62L3 21"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />{" "}
+                <path
+                  d="M19.5695 8.42999C20.4801 9.55186 21.2931 10.7496 21.9995 12.01C17.9995 19.01 13.2695 21.4 8.76953 19.23"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />{" "}
+              </g>
+            </svg>
+            }
+              </div>
               <input
+              ref={eye}
               placeholder="Enter your password"
                onChange={(e)=>{setPassword(e.target.value)}}
                 type="password"
@@ -214,18 +278,7 @@ export const LoginPage = () => {
                 required
                 aria-describedby="password-error"
               />
-              <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                <svg
-                  className="size-5 text-red-500"
-                  width={16}
-                  height={16}
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                  aria-hidden="true"
-                >
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                </svg>
-              </div>
+
             </div>
             {
               wrongCredentials && <>
